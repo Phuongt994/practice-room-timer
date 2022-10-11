e191196a82e63b1066488a8ebbc6a487b92a5937
Phuong Anh Tran <anh@anhs-mbp.lan>
2022-10-11 12:59:30 -0500

    add timer start end in minutes

--- timer.py
+++ timer.py
@@ -1,6 +1,9 @@
 timer_duration = 0
 
 timer_entry_title = input("Enter title: ")
-timer_entry_duration = input("Enter duration: ")
+timer_entry_start = float(input("Enter start time in 24hr [hh.mm] format e.g. 12.20: "))
+timer_entry_end = float(input("Enter end time in 24hr [hh.mm] format e.g. 12.50: "))
+
+timer_entry_duration = int((timer_entry_end - timer_entry_start) * 100)
 
 print(f"This entry is for {timer_entry_title} in {timer_entry_duration} minutes")
