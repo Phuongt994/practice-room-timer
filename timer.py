9fb49f5cf9beec845e460b13a1ce043a066932dc
Phuong Anh Tran <anh@anhs-mbp.lan>
2022-10-11 15:23:17 -0500

    add datetime for duration calc:

--- timer.py
+++ timer.py
@@ -1,9 +1,13 @@
+from datetime import datetime 
+
 timer_duration = 0
 
 timer_entry_title = input("Enter title: ")
-timer_entry_start = float(input("Enter start time in 24hr [hh.mm] format e.g. 12.20: "))
-timer_entry_end = float(input("Enter end time in 24hr [hh.mm] format e.g. 12.50: "))
+timer_entry_start = input("Enter start time in 24hr [hh:mm] format e.g. 12:20: ")
+timer_entry_end = input("Enter end time in 24hr [hh.mm] format e.g. 12:50: ")
 
-timer_entry_duration = int((timer_entry_end - timer_entry_start) * 100)
+timer_entry_duration = datetime.strptime(timer_entry_end, "%H:%M") - datetime.strptime(timer_entry_start, "%H:%M") 
+
+# timer_entry_duration = time(int(timer_entry_end[0]), int(timer_entry_end[1])) - time(int(timer_entry_start[0]), int(timer_entry_start[1])) 
 
 print(f"This entry is for {timer_entry_title} in {timer_entry_duration} minutes")
