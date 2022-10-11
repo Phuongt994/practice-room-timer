256fc2977c3753e1d2812a9118df7ad2b5b856db
Phuong Anh Tran <anh@anhs-mbp.lan>
2022-10-11 12:45:49 -0500

    Add timer entry

---
+++ timer.py
@@ -0,0 +1,6 @@
+timer_duration = 0
+
+timer_entry_title = input("Enter title: ")
+timer_entry_duration = input("Enter duration: ")
+
+print(f"This entry is for {timer_entry_title} in {timer_entry_duration} minutes")
